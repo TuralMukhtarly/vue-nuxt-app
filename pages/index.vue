bc3c331b29ad3acfda54edc7a3be3dfb5dc12e47
Tural <65925863+TuralMukhtarly@users.noreply.github.com>
2022-04-09 17:13:41 +0300

    create app | add style | add create task and add remove task

---
+++ pages/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div class="main">
+      <div class="header">
+        <img src="../assets/img/illustration.png" alt="" />
+      </div>
+      <div class="title">Today I need to</div>
+      <div style="display: flex" class="inp">
+        <input
+          type="text"
+          placeholder="Add new todo..."
+          v-model="newTask"
+          @keypress.enter="addTask"
+        />
+        <button @click="addTask">Submit</button>
+      </div>
+
+      <div class="todos">
+        <div class="todo" v-for="(task, i) in $store.state.tasks" :key="i">
+          <img src="../assets/img/frame.png" alt="" style="background: white" />
+          <input type="checkbox" name="" id="" :checked="task.done" />
+          <span style="background: white">{{ task.content }}</span>
+          <img
+            style="background: white"
+            src="../assets/img/pencil.png"
+            alt=""
+          />
+          <img
+            style="background: white"
+            src="../assets/img/bin.png"
+            alt=""
+            @click="removeTask(task)"
+          />
+        </div>
+      </div>
+
+      <div class="completedBlock">
+        <div>
+          <div>3 tasks</div>
+          <div>Completed</div>
+          <hr />
+        </div>
+
+        <div>
+          <div>0 tasks</div>
+          <div>To be finished</div>
+          <hr />
+        </div>
+      </div>
+
+      <div style="display: flex" class="chekedBtn">
+        <button @click="checkAll">Check all</button>
+        <button>All</button>
+        <button>Active</button>
+        <button>Complited</button>
+        <button>Clear Complited</button>
+      </div>
+
+      <!-- <div class="footer">
+        <img
+          src="../assets/img/checkmark.png"
+          alt=""
+          style="background: white"
+        />
+        <span style="background: white"
+          >Congrat, you have no more tasks to do</span
+        >
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      newTask: '',
+    }
+  },
+  methods: {
+    addTask() {
+      if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask)
+        this.newTask = ''
+      }
+    },
+    removeTask(task) {
+      this.$store.commit('REMOVE_TASK', task)
+    },
+    toggleDone(task) {
+      this.$store.commit('TOGGLE_TAK', task)
+    },
+    checkAll() {
+      this.$store.commit('CHECK_ALL')
+    },
+  },
+}
+</script>
